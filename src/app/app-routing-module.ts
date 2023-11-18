7a51d3bb19b9a3e66fd5f95e603c66d952c65375
Saksham Gupta <saksham757gupta@gmail.com>
2023-11-18 12:22:07 +0530

    added recipes edit

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,26 +4,35 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'recipes',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'recipes',
     component: RecipesComponent,
-    children:[
+    children: [
       {
         path: '',
-        component: RecipesStartComponent
+        component: RecipesStartComponent,
+      },
+      {
+        path: 'new',
+        component: RecipeEditComponent,
       },
       {
         path: ':id',
-        component: RecipeDetailComponent
-      }
-    ]
+        component: RecipeDetailComponent,
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+      },
+    ],
   },
   {
     path: 'shopping-list',
