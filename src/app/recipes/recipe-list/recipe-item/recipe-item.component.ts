ecb59474dc3437e2e87e03dd825f211c8a50c35f
Saksham Gupta <saksham757gupta@gmail.com>
2023-11-18 11:32:52 +0530

    added child routing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,22 +2,22 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 import { RecipeService } from '../../recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private router: Router, private recipeService: RecipeService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
+    const recipeIndex = this.recipeService.findRecipeIndex(this.recipe);
+    this.router.navigate(['recipes', recipeIndex+1]);
   }
-
 }
